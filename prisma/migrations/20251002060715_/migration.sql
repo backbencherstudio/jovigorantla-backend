2265612a5a57bbcf0294e109d465b48a943b2afe
ziauddin128 <ziauddindev2000@gmail.com>
2025-10-02 14:59:46 +0600

    name-change-fix

---
+++ prisma/migrations/20251002060715_/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `username_change_count` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `username_change_date` on the `users` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "users" 
+-- DROP COLUMN "username_change_count",
+-- DROP COLUMN "username_change_date",
+ADD COLUMN     "name_change_count" INTEGER NOT NULL DEFAULT 0,
+ADD COLUMN     "name_change_date" TIMESTAMPTZ;
