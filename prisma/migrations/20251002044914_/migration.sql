ff0d461135507f2a44f4fe0f8d83998e674bf3f5
ziauddin128 <ziauddindev2000@gmail.com>
2025-10-02 15:10:30 +0600

    migration-file-deleted

--- prisma/migrations/20251002044914_/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `username_change_date` to the `users` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "users" ADD COLUMN     "username_change_count" INTEGER NOT NULL DEFAULT 0,
-ADD COLUMN     "username_change_date" TIMESTAMPTZ NOT NULL;
